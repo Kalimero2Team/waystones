5aafd566ed71e4dc5c4055a291db5146bf947495
byquanton <32410361+byquanton@users.noreply.github.com>
2022-10-31 22:44:59 +0100

    Fix Bedrock GUI

--- waystones-paper/src/main/java/com/kalimero2/waystones/paper/BedrockCompat.java
+++ waystones-paper/src/main/java/com/kalimero2/waystones/paper/BedrockCompat.java
@@ -4,6 +4,8 @@ import com.jeff_media.customblockdata.CustomBlockData;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.geysermc.cumulus.SimpleForm;
+import org.geysermc.cumulus.component.ButtonComponent;
+import org.geysermc.cumulus.response.SimpleFormResponse;
 import org.geysermc.floodgate.api.FloodgateApi;
 import org.geysermc.floodgate.api.player.FloodgatePlayer;
 
@@ -23,13 +25,18 @@ public class BedrockCompat {
             Location location = entry.getValue();
             CustomBlockData customBlockData = new CustomBlockData(location.getBlock(), PaperWayStones.plugin);
             SerializableWayStone wayStone = customBlockData.get(PaperWayStones.WAYSTONE_KEY, WayStoneDataTypes.WAY_STONE);
-            hashMap.put(integer+" - "+wayStone.getName(), integer);
-            builder.button(integer+" - "+wayStone.getName());
+            if(wayStone != null){
+                hashMap.put(integer+" - "+wayStone.getName(), integer);
+                builder.button(integer + " - " + wayStone.getName());
+            }
         }
 
-        builder.responseHandler((simpleForm, response) -> {
-            if(hashMap.containsKey(response)){
-                player.chat("/waystone tp "+hashMap.get(response));
+        builder.responseHandler((form, responseData) -> {
+            SimpleFormResponse response = form.parseResponse(responseData);
+
+            ButtonComponent button = response.getClickedButton();
+            if(button != null && hashMap.containsKey(button.getText())){
+                player.chat("/waystone tp "+hashMap.get(button.getText()));
             }
         });
 
