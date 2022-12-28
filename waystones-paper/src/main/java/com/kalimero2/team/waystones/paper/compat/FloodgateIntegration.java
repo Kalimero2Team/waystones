7357fe6448897f1345a45c293a01194a3ff16ee5
Quantum625 <linpec@protonmail.com>
2022-12-28 22:45:03 +0100

    Added a button in the Bedrock UI to change Favorites

--- waystones-paper/src/main/java/com/kalimero2/team/waystones/paper/compat/FloodgateIntegration.java
+++ waystones-paper/src/main/java/com/kalimero2/team/waystones/paper/compat/FloodgateIntegration.java
@@ -5,8 +5,11 @@ import com.kalimero2.team.waystones.paper.PaperWayStones;
 import com.kalimero2.team.waystones.paper.SerializableWayStone;
 import com.kalimero2.team.waystones.paper.SerializableWayStones;
 import com.kalimero2.team.waystones.paper.WayStoneDataTypes;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataType;
 import org.geysermc.cumulus.SimpleForm;
 import org.geysermc.cumulus.component.ButtonComponent;
 import org.geysermc.cumulus.response.SimpleFormResponse;
@@ -24,14 +27,24 @@ public class FloodgateIntegration {
         SerializableWayStones wayStones = PaperWayStones.plugin.getSerializableWayStones(player.getWorld());
         HashMap<String, Integer> hashMap = new HashMap<>();
 
+        builder.button("§6★ Favoriten ändern ★");
+
         for (Map.Entry<Integer, Location> entry : wayStones.getWayStones().entrySet()) {
             Integer integer = entry.getKey();
             Location location = entry.getValue();
             CustomBlockData customBlockData = new CustomBlockData(location.getBlock(), PaperWayStones.plugin);
             SerializableWayStone wayStone = customBlockData.get(PaperWayStones.WAYSTONE_KEY, WayStoneDataTypes.WAY_STONE);
             if(wayStone != null){
-                hashMap.put(integer+" - "+wayStone.getName(), integer);
-                builder.button(integer + " - " + wayStone.getName());
+                String color = "§r";
+                if (player.getPersistentDataContainer().has(new NamespacedKey("waystones", "favourite_waystones"))) {
+                    for (int id : player.getPersistentDataContainer().get(new NamespacedKey("waystones", "favourite_waystones"), PersistentDataType.INTEGER_ARRAY)) {
+                        if (id == integer) {
+                            color = "§6";
+                        }
+                    }
+                }
+                hashMap.put(color + "[★]  §r" + integer+" - "+wayStone.getName(), integer);
+                builder.button(color + "[★]  §r" + integer + " - " + wayStone.getName());
             }
         }
 
@@ -39,8 +52,13 @@ public class FloodgateIntegration {
             SimpleFormResponse response = form.parseResponse(responseData);
 
             ButtonComponent button = response.getClickedButton();
-            if(button != null && hashMap.containsKey(button.getText())){
-                player.chat("/waystone tp "+hashMap.get(button.getText()));
+            if(button != null){
+                if (hashMap.containsKey(button.getText())) {
+                    player.chat("/waystone tp " + hashMap.get(button.getText()));
+                }
+                else {
+                    player.chat("/waystone switchtofavoritemenu");
+                }
             }
         });
 
