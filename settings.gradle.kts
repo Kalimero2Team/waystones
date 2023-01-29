8beb60ba5a613eea69216654683b6f8685904ca0
byquanton <32410361+byquanton@users.noreply.github.com>
2023-01-29 17:23:10 +0100

    Update Cloud

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ dependencyResolutionManagement{
             version("geyser-baseApi","2.1.0-byquanton-SNAPSHOT")
             version("geyser-geyserApi","2.1.0-byquanton-SNAPSHOT")
             version("geyser-geyserCore","2.1.0-byquanton-SNAPSHOT")
-            version("cloud", "1.7.1")
+            version("cloud", "1.8.0")
             version("customblockdata","2.1.0")
             version("morepersistentdatatypes","2.3.1")
             version("anvilgui","1.5.3-SNAPSHOT")
