a09d985d9606bab1917163c227c296702a513a8c
byquanton <32410361+byquanton@users.noreply.github.com>
2023-12-29 01:25:27 +0100

    ergregergeg

---
+++ waystones-paper/src/main/java/com/kalimero2/team/waystones/paper/ui/screen/InputScreen.java
@@ -0,0 +1,13 @@
+package com.kalimero2.team.waystones.paper.ui.screen;
+
+import org.bukkit.entity.Player;
+
+public class InputScreen implements GenericScreen {
+
+
+
+    @Override
+    public void open(Player player) {
+
+    }
+}
