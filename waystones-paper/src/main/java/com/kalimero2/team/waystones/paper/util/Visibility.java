70dab4650b3c0698e29df566caebf6ff39e634ef
byquanton <32410361+byquanton@users.noreply.github.com>
2023-12-31 14:41:37 +0100

    Use Enums as they were intended

--- waystones-paper/src/main/java/com/kalimero2/team/waystones/paper/util/Visibility.java
+++ waystones-paper/src/main/java/com/kalimero2/team/waystones/paper/util/Visibility.java
@@ -6,30 +6,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Visibility {
-    PUBLIC,
-    UNLISTED,
-    PRIVATE;
+    PUBLIC(0, "waystones.visibility.public"),
+    UNLISTED(1, "waystones.visibility.unlisted"),
+    PRIVATE(2, "waystones.visibility.private");
 
     private static final Map<Integer, Visibility> MAP = new HashMap<>();
-    private static final Component[] TEXT = {Component.translatable("waystones.visibility.public"), Component.translatable("waystones.visibility.unlisted"), Component.translatable("waystones.visibility.private")};
 
     static {
         for (Visibility element : values()) {
-            MAP.put(element.ordinal(), element);
+            MAP.put(element.id(), element);
         }
+    }
+
+    private final int id;
+    private final String translationKey;
+
+    Visibility(int id, String translationKey) {
+        this.id = id;
+        this.translationKey = translationKey;
     }
 
 
     public static Visibility valueByNumber(int id) {
         try {
             return MAP.get(id);
-        }
-        catch (Exception e) {
+        } catch (NullPointerException e) {
             return PUBLIC;
         }
     }
 
+    public int id() {
+        return id;
+    }
+
     public Component text() {
-        return TEXT[ordinal()];
+        return Component.translatable(translationKey);
     }
 }
