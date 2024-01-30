Kalimero2Team/waystones

commit aa1a46a0fa8e23691ce5b0ea0e0391c40680bcf8

Fixed that renamed portable waystones didn't work

waystones-paper/src/main/java/com/kalimero2/team/waystones/paper/command/WayStoneCommands.java
@@ -32,6 +32,7 @@ import org.bukkit.block.Block;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.List;
@@ -261,13 +262,18 @@ public class WayStoneCommands extends CommandHandler {
             }
 
             if (!teleportAllowed) {
-                if (player.getInventory().containsAtLeast(plugin.getPortable(), 1)) {
-                    if (player.getLevel() >= 1) {
-                        player.setLevel(player.getLevel() - 1);
-                        teleportAllowed = true;
-                    } else {
-                        player.sendMessage(Component.translatable("waystones.teleport.noxp", TextUtil.ORANGE));
-                        return;
+                for (ItemStack stack : player.getInventory().getContents()) {
+                    if (stack != null) {
+                        if (stack.getItemMeta().getPersistentDataContainer().has(new NamespacedKey("waystones", "portable"))) {
+                            if (player.getLevel() >= 1) {
+                                player.setLevel(player.getLevel() - 1);
+                                teleportAllowed = true;
+                            } else {
+                                player.sendMessage(Component.translatable("waystones.teleport.noxp", TextUtil.ORANGE));
+                                return;
+                            }
+                            break;
+                        }
                     }
                 }
             }

waystones-paper/src/main/java/com/kalimero2/team/waystones/paper/ui/NewScreens.java
@@ -151,7 +151,6 @@ public class NewScreens {
             } else {
                 player.sendMessage(Component.text("Waystone konnte nicht entfernt werden.", TextUtil.RED));
             }
-
             player.closeInventory();
         });
         builder.button(new ButtonScreen.Button(Component.text("Abbrechen"), 3, 4), HumanEntity::closeInventory);

commit b1b8f8e025d3a87f5462ca64e6204893e245b1b8

Fix: Regression from aa1a46a0fa8e23691ce5b0ea0e0391c40680bcf8

waystones-paper/src/main/java/com/kalimero2/team/waystones/paper/command/WayStoneCommands.java
@@ -264,7 +264,7 @@ public class WayStoneCommands extends CommandHandler {
             if (!teleportAllowed) {
                 for (ItemStack stack : player.getInventory().getContents()) {
                     if (stack != null) {
-                        if (stack.getItemMeta().getPersistentDataContainer().has(new NamespacedKey("waystones", "portable"))) {
+                        if (stack.getItemMeta().getPersistentDataContainer().has(new NamespacedKey(plugin, "portable"))) {
                             if (player.getLevel() >= 1) {
                                 player.setLevel(player.getLevel() - 1);
                                 teleportAllowed = true;